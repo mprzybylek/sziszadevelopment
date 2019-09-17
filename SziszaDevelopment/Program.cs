354f837fca4fe6d62eeeaeb8685a8b19af769b54
Pawel Szuber <pawel.szuber@aon.pl>
2019-09-17 11:28:21 +0200

    Utworzenie struktury projektu

---
+++ SziszaDevelopment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SziszaDevelopment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

28cc123357312309a87985f9af76fc9bb8b0debd
GrzegorzTokarski <grztok@gmail.com>
2019-10-01 12:02:35 +0200

    static keyword

---
+++ MethodBasics/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodBasics
+{
+    class Person
+    {
+        private int _age;
+
+        private static int _personCounter;
+
+        public static void IncrementCounter()
+        {
+            _personCounter++;
+        }
+
+        public static int ReturnCounter()
+        {
+            return _personCounter;
+        }
+
+        public static void DisplayCounter()
+        {
+            Console.WriteLine($"Current value: {_personCounter}" + ".");
+        }
+
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+
+        public void DisplayAge()
+        {
+            Console.WriteLine("Age: " + _age + ".");
+        }
+    }
+}
--- MethodBasics/Program.cs
+++ MethodBasics/Program.cs
@@ -6,17 +6,47 @@ namespace MethodBasics
     {
         static void Main(string[] args)
         {
-            User user = new User();
+            //User user = new User();
 
-            user.ShowName();
+            //user.ShowName();
             //user.ShowProffession();  -  inaccessible level 
             //user.sex = "other";  - inaccessible level 
 
 
-            user.ShowAddress();
-            
+            //user.ShowAddress();
+
+            //wywolanie statycznej metody DisplayCounter()
+            Person.DisplayCounter();
+            //stworzenie nowego obiektu typu Person i przypisanie do zmiennej typu Person
+            Person mirella = new Person();
+            //
+            Person.IncrementCounter();
+
+            mirella.SetAge(18);
+
+            Person.DisplayCounter();
+
+
+
+
+            Person cecylia;
+            cecylia = new Person();
+
+            Person.IncrementCounter();
+
+            cecylia.SetAge(19);
+
+            Person.DisplayCounter();
+
+            mirella.DisplayAge();
+            cecylia.DisplayAge();
+
+            Console.WriteLine($"Current counter: {Person.ReturnCounter()}");
 
             Console.ReadKey();
+
+
+
 
         }
     }

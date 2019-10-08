827ec962781faa464286e8dbeb004ad32fc8cab9
GrzegorzTokarski <grztok@gmail.com>
2019-10-08 13:28:01 +0200

    Stack vs Heap

---
+++ Stack vs Heap/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_vs_Heap
+{
+    class MyClass
+    {
+        public int value;
+
+        public MyClass(int valueP)
+        {
+            value = valueP;
+            //Console.WriteLine("This is MyClass constructor");
+        }
+
+    }
+}
---
+++ Stack vs Heap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stack_vs_Heap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valueToChange = 15;
+
+            MyClass myClass1 = new MyClass(8);
+            MyClass myClass2 = new MyClass(10);
+            myClass1 = myClass2;
+            myClass2.value = 15;
+
+            ChangeValue(myClass1);
+            ChangeValue(valueToChange);
+            
+            Console.WriteLine($"{myClass1.value}");
+            Console.WriteLine($"{valueToChange}");
+            //Console.ReadKey();
+        }
+
+        public static void ChangeValue(int val)
+        {
+            val = 666;
+        }
+
+        public static void ChangeValue(MyClass val)
+        {
+            val.value = 666;
+        }
+    }
+}
